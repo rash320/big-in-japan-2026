0968794c86a1d9302c386682a7515765b888ded1
rash320 <82438621+rash320@users.noreply.github.com>
2026-09-06 18:11:06 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.registration.unregister(),caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x))))])));
